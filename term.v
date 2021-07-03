e9dd35094574ee71d40f25ae9d63aa2d67dde148
Molly Stewart-Gallus <stevenselectronicmail@gmail.com>
2021-07-02 20:04:55 -0700

    Cleanup cylinder variables as well

--- term.v
+++ term.v
@@ -5,14 +5,20 @@ Import ListNotations.
 
 Close Scope nat.
 
-Definition α := nat.
-Inductive sort :=
+Inductive sort α :=
 | unit
-| prod (A B: sort)
-| exp (s t: sort)
-| pos (κ:α) (A: sort)
-| nec (κ:α) (A: sort)
+| prod (A B: sort α)
+| exp (s t: sort α)
+| pos (κ: α) (A: sort α)
+| nec (κ: α) (A: sort α)
 | eq (κ μ: α).
+
+Arguments unit {α}.
+Arguments prod {α}.
+Arguments exp {α}.
+Arguments pos {α}.
+Arguments nec {α}.
+Arguments eq {α}.
 
 Notation "[ A , B ]" := (exp A B).
 Infix "×" := prod (at level 50).
@@ -23,64 +29,64 @@ Infix "~" := eq (at level 90).
 
 Reserved Notation "f ∘ g" (at level 30).
 
-Inductive tm v :=
+Inductive tm v α :=
 | var (x: v)
 
-| lam (x: v) (s: sort) (e: tm v)
-| app (f x: tm v)
+| lam (x: v) (s: sort α) (e: tm v α)
+| app (f x: tm v α)
 
 | tt
 
-| fanout (e0 e1: tm v)
-| π1 (e: tm v)
-| π2 (e: tm v)
+| fanout (e0 e1: tm v α)
+| π1 (e: tm v α)
+| π2 (e: tm v α)
 
-| necessity (κ: α) (e: tm v)
-| ext (κ: α) (e: tm v)
-| dup (e: tm v)
+| necessity (κ: α) (e: tm v α)
+| ext (κ: α) (e: tm v α)
+| dup (e: tm v α)
 
-| box (κ: α) (e: tm v)
-| bind (e0: tm v) (x: v) (e1: tm v)
+| box (κ: α) (e: tm v α)
+| bind (e0: tm v α) (x: v) (e1: tm v α)
 
 | id (κ: α)
-| compose (e0 e1: tm v)
-| sym (e: tm v)
+| compose (e0 e1: tm v α)
+| sym (e: tm v α)
 .
 
-Arguments var {v}.
+Arguments var {v α}.
 
-Arguments lam {v}.
-Arguments app {v}.
+Arguments lam {v α}.
+Arguments app {v α}.
 
-Arguments tt {v}.
+Arguments tt {v α}.
 
-Arguments fanout {v}.
-Arguments π1 {v}.
-Arguments π2 {v}.
+Arguments fanout {v α}.
+Arguments π1 {v α}.
+Arguments π2 {v α}.
 
-Arguments necessity {v}.
-Arguments ext {v}.
-Arguments dup {v}.
+Arguments necessity {v α}.
+Arguments ext {v α}.
+Arguments dup {v α}.
 
-Arguments box {v}.
-Arguments bind {v}.
+Arguments box {v α}.
+Arguments bind {v α}.
 
-Arguments id {v}.
-Arguments compose {v}.
-Arguments sym {v}.
+Arguments id {v α}.
+Arguments compose {v α}.
+Arguments sym {v α}.
 
 Infix "∘" := compose.
 Notation "⟨ x , y , .. , z ⟩" := (fanout .. (fanout x y) .. z).
 
-Record ofty v := ofty_intro {
-  Γ: list (v * sort) ;
-  e: tm v ;
-  τ: sort ;
+Record ofty v α := ofty_intro {
+  Γ: list (v * sort α) ;
+  e: tm v α;
+  τ: sort α;
 }.
-Arguments ofty_intro {v}.
-Arguments Γ {v}.
-Arguments e {v}.
-Arguments τ {v}.
+Arguments ofty_intro {v α}.
+Arguments Γ {v α}.
+Arguments e {v α}.
+Arguments τ {v α}.
 Notation "Γ ⊢ e ∈ τ" := (ofty_intro Γ e τ) (at level 80).
 
 Record bundle A := sup {
@@ -135,7 +141,7 @@ Variant rule :=
 | judge_sym
 .
 
-Definition judge v (r: rule): bundle (prop (ofty v)) :=
+Definition judge v α (r: rule): bundle (prop (ofty v α)) :=
   match r with
   | judge_var_head =>
     sup '(Γ, τ, x), {
@@ -243,43 +249,51 @@ Definition judge v (r: rule): bundle (prop (ofty v)) :=
     }
   end.
 
-Inductive proof v: list (ofty v) → Type :=
-| QED: proof v []
+(* Inductive forest v := *)
+(* | mt *)
+(* | premise: list (ofty v) → forest v → forest v. *)
+
+Inductive proof v α: list (ofty v α) → Type :=
+| QED: proof v α []
 | step
     {T}
     (r: rule)
-    (s: judge v r)
+    (s: judge v α r)
   :
-    proof v (tail (π (judge v r) s) ++ T) →
-    proof v (head (π (judge v r) s) :: T).
+    proof v α (tail (π (judge v α r) s) ++ T) →
+    proof v α (head (π (judge v α r) s) :: T).
 
-Arguments QED {v}.
-Arguments step {v T}.
+Arguments QED {v α}.
+Arguments step {v α T}.
 
 Notation "A # B ; C" := (step A B C) (at level 60, right associativity,
                                       format "A  #  B  ; '//' C").
 
-Example proof_tt v: proof v [[] ⊢ tt ∈ unit] :=
+Example proof_tt v α: proof v α [[] ⊢ tt ∈ unit] :=
   judge_tt # _ ;
   QED.
 
-Example proof_compose v κ: proof v [[] ⊢ id κ ∘ id κ ∈ (κ ~ κ)]
+Example proof_compose v α κ: proof v α [[] ⊢ id κ ∘ id κ ∈ (κ ~ κ)]
   :=
     judge_compose # ([], κ, κ, κ, id κ, id κ) ;
     judge_id # ([], κ) ;
     judge_id # ([], κ) ;
     QED.
 
-Example proof_id v x A: proof v [[] ⊢ lam x A (var x) ∈ [A, A]]
+Example proof_id v α x A: proof v α [[] ⊢ lam x A (var x) ∈ [A, A]]
   :=
   judge_lam # ([], A, A, x, var x) ;
   judge_var_head # ([], A, x) ;
   QED.
 
-Example proof_const v x y A B: proof v [[] ⊢ lam x A (lam y B (var x)) ∈ [A, [B, A]]]
-:=
-judge_lam # ([], A, [B, A], x, lam y B (var x)) ;
-judge_lam # ([(x, A)], B, A, y, var x) ;
-judge_var_tail # ([(x, A)], (y, B), A, x) ;
-judge_var_head # ([(x, A)], A, x) ;
-QED.
+Example proof_const v α x y A B: proof v α [[] ⊢ lam x A (lam y B (var x)) ∈ [A, [B, A]]] :=
+  judge_lam # ([], A, [B, A], x, lam y B (var x)) ;
+  judge_lam # ([(x, A)], B, A, y, var x) ;
+  judge_var_tail # ([(x, A)], (y, B), A, x) ;
+  judge_var_head # ([(x, A)], A, x) ;
+  QED.
+
+Example proof_nec_tt v α κ: proof v α [[] ⊢ necessity κ tt ∈ □ κ, unit] :=
+  judge_necessity # ([], κ, unit, tt) ;
+  judge_tt # [] ;
+  QED.
