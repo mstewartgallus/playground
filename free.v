1ede83271cd2d3e84ef6974b74edc6a83771c951
Molly Stewart-Gallus <stevenselectronicmail@gmail.com>
2021-07-18 21:03:45 -0700

    Rework to be more categorical

--- free.v
+++ free.v
@@ -2,89 +2,90 @@ Require Import Coq.Unicode.Utf8.
 Require Import Coq.Setoids.Setoid.
 Require Import Coq.Classes.SetoidClass.
 
-Inductive free {X} :=
+Declare Scope category_scope.
+Declare Scope functor_scope.
+Declare Scope morphism_scope.
+
+Delimit Scope category_scope with category.
+Delimit Scope functor_scope with functor.
+Delimit Scope morphism_scope with morphism.
+
+Inductive category {X} :=
 | η (x: X)
-| top
-| bot
-| impl (A B: free)
-| and (A B: free)
-| or (A B: free)
-| path (A B: free)
+| set
+| trv
+| prod (A B: category)
 .
 
-Arguments free: clear implicits.
-Notation "⊤" := top.
-Notation "⊥" := bot.
-Infix "∧" := and.
-Infix "∨" := or.
-Notation "[ A , B ]" := (impl A B).
+Arguments category: clear implicits.
+Bind Scope category_scope with category.
 
-Infix "=" := path.
+Notation "⊤" := trv.
+Infix "*" := prod.
+
+Reserved Notation "A ⊩ B" (at level 80).
+
+Inductive functor {X}: category X → category X → Type :=
+| Id A: A ⊩ A
+| Compose {A B C}:
+    B ⊩ C → A ⊩ B → A ⊩ C
+
+| Bang A: A ⊩ ⊤
+| Fanout {A B C}:
+    C ⊩ A → (C ⊩ B) →
+    C ⊩ (A * B)
+| Fst A B: (A * B) ⊩ A
+| Snd A B: (A * B) ⊩ B
+
+| Unit: ⊤ ⊩ set
+| Empty: ⊤ ⊩ set
+| Prod: (set * set) ⊩ set
+| Sum: (set * set) ⊩ set
+where "A ⊩ B" := (functor A B)
+.
+
+Bind Scope functor_scope with functor.
+
+Infix "∘" := Compose (at level 30) : functor_scope.
+
+Notation "!" := Bang : functor_scope.
+Notation "⟨ x , y , .. , z ⟩" := (Fanout .. (Fanout x y) .. z) : functor_scope.
+
+Notation "⊤" := Unit : functor_scope.
+Notation "⊥" := Empty : functor_scope.
 
 Reserved Notation "A ⊢ B" (at level 80).
 
-Inductive entails {X}: free X → free X → Type :=
-| id A: A ⊢ A
-| compose {A B C}:
+Inductive morphism {X}: ∀ {F G: category X}, F ⊩ G → F ⊩ G → Type :=
+| id {F G} (A: F ⊩ G) : A ⊢ A
+| compose {F G} {A B C: F ⊩ G}:
     B ⊢ C → A ⊢ B → A ⊢ C
 
-| bang A: A ⊢ ⊤
-| fanout {A B C}:
-    C ⊢ A → (C ⊢ B) →
-    C ⊢ (A ∧ B)
-| π1 A B: (A ∧ B) ⊢ A
-| π2 A B: (A ∧ B) ⊢ B
+| bang {Γ} (A: Γ ⊩ set): A ⊢ (Unit ∘ ! _)
+| fanout {Γ} {C A B: Γ ⊩ set}:
+    C ⊢ A → C ⊢ B →
+    C ⊢ Prod ∘ ⟨A, B⟩
+| fst {Γ} {A B: Γ ⊩ set}:
+    Prod ∘ ⟨A, B⟩ ⊢ A
+| snd {Γ} {A B: Γ ⊩ set}:
+    Prod ∘ ⟨A, B⟩ ⊢ B
 
-| absurd A: ⊥ ⊢ A
-| fanin {A B C}:
-    A ⊢ C → (B ⊢ C) →
-    (A ∨ B) ⊢ C
-| i1 A B: A ⊢ (A ∨ B)
-| i2 A B: B ⊢ (A ∨ B)
-
-| cur {A B C}:
-    (A ∧ B) ⊢ C → B ⊢ [A, C]
-| ev A B:
-    (A ∧ [A,  B]) ⊢ B
-
-| refl A: ⊤ ⊢ (A = A)
-| trans {A B C}:
-    ((B = C) ∧ (A = B)) ⊢ (A = C)
-| sym {A B} : (A = B) ⊢ (B = A)
-
-(* FIXME in general not well-founded *)
-| J {x y} P:
-    (x = y ∧ P x) ⊢ P y
-where "A ⊢ B" := (entails A B)
+| absurd {Γ} (A: Γ ⊩ set): (Empty ∘ ! _) ⊢ A
+| fanin {Γ} {A B C: Γ ⊩ set}:
+    A ⊢ C → B ⊢ C →
+    Sum ∘ ⟨A,  B⟩ ⊢ C
+| inl {Γ} {A B: Γ ⊩ set}: A ⊢ Sum ∘ ⟨A, B⟩
+| inr {Γ} {A B: Γ ⊩ set}: B ⊢ Sum ∘ ⟨A, B⟩
+where "A ⊢ B" := (morphism A B)
 .
 
-Infix "∘" := compose (at level 30).
-Notation "!" := bang.
-Notation "⁻¹" := sym.
+Bind Scope morphism_scope with morphism.
 
-Notation "⟨ x , y , .. , z ⟩" := (fanout .. (fanout x y) .. z).
-Notation "[ x ; y ; .. ; z ⟩" := (fanin .. (fanin x y) .. z).
+Infix "∘" := compose : morphism_scope.
+Notation "!" := bang : morphism_scope.
 
-Definition simple {A} (x y: free A) (p: ⊤ ⊢ x = y) (q: ⊤ ⊢ x): ⊤ ⊢ y.
-Proof.
-  apply (J (λ x, x) ∘ ⟨ p, q ⟩).
-Defined.
+Notation "⟨ x , y , .. , z ⟩" := (fanout .. (fanout x y) .. z) : morphism_scope.
+Notation "[ x ; y ; .. ; z ]" := (fanin .. (fanin x y) .. z) : morphism_scope.
 
-Definition reverse {A} (x y: free A) (p: ⊤ ⊢ y = x) (q: ⊤ ⊢ x): ⊤ ⊢ y.
-Proof.
-  apply (J (λ x, x) ∘ ⟨ ⁻¹ ∘ p, q ⟩).
-Defined.
-
-Require Import Coq.Strings.String.
-
-Function subst x s ev :=
-  match ev with
-  | η y => if string_dec x y then s else ev
-  | [A, B] => [subst x s A, subst x s B]
-  | A ∧ B => subst x s A ∧ subst x s B
-  | A ∨ B => subst x s A ∨ subst x s B
-  | A = B => subst x s A = subst x s B
-  | _ => ev
-  end.
-
-Notation "'[' x := u ']' v" := (subst x u v) (at level 1).
+Example diag {X} {Γ: category X} (A: Γ ⊩ set): A ⊢ Prod ∘ ⟨A, A⟩ :=
+  ⟨ id _,  id _ ⟩.
